40d0605206b0ab2c9608e4822ed0e1e56239bdb8
1161780819 <xie_zhongyang@qq.com>
2017-10-11 15:54:11 +0800

    实现wordcount并回写到hdfs

--- src/main/scala/com/yang/spark/SparkWordCount.scala
+++ src/main/scala/com/yang/spark/SparkWordCount.scala
@@ -4,10 +4,8 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by yang on 2017/10/11.
-  * spark程序读取hdfs文件
   */
-object SparkReadHdfs {
-
+object SparkWordCount {
   def main(args: Array[String]): Unit = {
     if (args.length < 2){
       System.err.println("please infput the hdfs file path!")
@@ -16,13 +14,12 @@ object SparkReadHdfs {
 
     val sparkconf = new SparkConf().setAppName("ReadHdfs")
     val sc = new SparkContext(sparkconf)
-    val file = sc.textFile(args(0))
 
-    //file.saveAsTextFile(args(1))
-    //指定保存到单个文件中
-    file.repartition(1).saveAsTextFile(args(1))
+    val file = sc.textFile(args(0))
+    val words = file.flatMap(_.split(" ")).map((_,1)).reduceByKey(_ + _)
+
+    words.repartition(1).saveAsTextFile(args(1))
 
     sc.stop()
   }
-
 }
